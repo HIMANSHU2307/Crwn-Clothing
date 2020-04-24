54dc05061f99eeb53016141df8001c5f980c75cc
himanshu sharma <hvs2394@gmail.com>
2020-04-25 04:06:38 +0530

    Sign IN form created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/homepage/homepage.component';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop-page.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUp}/>
       </Switch>
       {/* 
         exact returs boolean, exact={true}, path is string, component takes a 
